14b569b55d8fd0fbb412a593c4af3ef137d1b463
Carolina Muñoz <carito.camo@gmail.com>
2021-07-08 00:37:19 -0400

    Ejercicios clases JS

---
+++ script2.js
@@ -0,0 +1,68 @@
+console.log ("13---ESCANDALOTRON--")
+console.log ("----grupo1----")
+let persona1 = 20;
+let persona2 = 10;
+let persona3 = 40;
+
+let totalDecibelios = persona1 + persona2 + persona3;
+
+if (totalDecibelios > 0 && totalDecibelios <= 30){
+    console.log ("Las personas están susurrando");
+}
+if (totalDecibelios > 30 && totalDecibelios <= 60) {
+    console.log ("El sonido es NORMAL");
+}
+if (totalDecibelios > 60) {
+    console.log ("Las personas están haciendo un ESCÁNDALO")
+}
+
+console.log ("----grupo2----")
+persona1 = 10;
+persona2 = 5;
+persona3 = 15;
+
+totalDecibelios = persona1 + persona2 + persona3;
+
+if (totalDecibelios > 0 && totalDecibelios <= 30){
+    console.log ("Las personas están susurrando");
+}
+if (totalDecibelios > 30 && totalDecibelios <= 60) {
+    console.log ("El sonido es NORMAL");
+}
+if (totalDecibelios > 60) {
+    console.log ("Las personas están haciendo un ESCÁNDALO")
+}
+
+console.log ("----grupo3----")
+persona1 = 26;
+persona2 = 50;
+persona3 = 99;
+
+totalDecibelios = persona1 + persona2 + persona3;
+
+if (totalDecibelios > 0 && totalDecibelios <= 30){
+    console.log ("Las personas están susurrando");
+}
+if (totalDecibelios > 30 && totalDecibelios <= 60) {
+    console.log ("El sonido es NORMAL");
+}
+if (totalDecibelios > 60) {
+    console.log ("Las personas están haciendo un ESCÁNDALO")
+}
+
+console.log ("----grupo4----")
+persona1 = 20;
+persona2 = 20;
+persona3 = 20;
+
+totalDecibelios = persona1 + persona2 + persona3;
+
+if (totalDecibelios > 0 && totalDecibelios <= 30){
+    console.log ("Las personas están susurrando");
+}
+if (totalDecibelios > 30 && totalDecibelios <= 60) {
+    console.log ("El sonido es NORMAL");
+}
+if (totalDecibelios > 60) {
+    console.log ("Las personas están haciendo un ESCÁNDALO")
+}
